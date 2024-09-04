ab90e23dc6d66b5126692ee1ef7fec6b0eb44bda
kiranwaskle <114229495+kiranwaskle@users.noreply.github.com>
2024-09-04 15:15:51 +0530

    Db.js

---
+++ Db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/inotebook";
+
+const connectToMongo = ()=>
+{
+    mongoose.connect(mongoURI).then(()=>
+     console.log("connected to mongo successfully!")).catch((e)=>console.log(e.message))
+}
+
+
+module.exports = connectToMongo
